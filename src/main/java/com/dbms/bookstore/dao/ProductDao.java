d571dcbf03f1840cd504106a7fea98ae02e3402f
Aryan <aryanitinpatil@gmail.com>
2023-10-29 15:25:52 +0530

    Create ProductDao.java

---
+++ src/main/java/com/dbms/bookstore/dao/ProductDao.java
@@ -0,0 +1,61 @@
+package com.dbms.bookstore.dao;
+
+import com.dbms.bookstore.model.Category;
+
+import lombok.Data;
+
+@Data
+public class ProductDao {
+	private Long id;
+	private String name;
+	private int categoryId;
+	private Double price;
+	private Double weight;
+	private String description;
+	private String imageName;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Double getWeight() {
+		return weight;
+	}
+	public void setWeight(Double weight) {
+		this.weight = weight;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImageName() {
+		return imageName;
+	}
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+	
+	
+	
+}
