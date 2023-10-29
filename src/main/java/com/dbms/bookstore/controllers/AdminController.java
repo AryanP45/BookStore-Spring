023f8ee4c10cab8ab99995f80dbc4c25877dae87
Aryan <aryanitinpatil@gmail.com>
2023-10-29 15:25:19 +0530

    added product routes

--- src/main/java/com/dbms/bookstore/controllers/AdminController.java
+++ src/main/java/com/dbms/bookstore/controllers/AdminController.java
@@ -1,6 +1,13 @@
 package com.dbms.bookstore.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,16 +16,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.dbms.bookstore.dao.ProductDao;
 import com.dbms.bookstore.model.Category;
+import com.dbms.bookstore.model.Product;
 import com.dbms.bookstore.services.CategoryService;
+import com.dbms.bookstore.services.ProductService;
 
 
 @Controller
 public class AdminController {
-	
+	public String uploadDir = System.getProperty("user.dir")+"\\src\\main\\resources\\static\\productImages";
 	@Autowired
 	CategoryService categoryService;
+	@Autowired
+	ProductService productService;
 	
 	@GetMapping("/admin")
 	public String adminHome() {
@@ -42,7 +56,7 @@ public class AdminController {
 		categoryService.addCategory(category);
 		return "redirect:/admin/categories";
 	}
-	
+
 	@GetMapping("/admin/categories/delete/{id}")
 	public String deleteCategory(@PathVariable int id) {
 		categoryService.removeCategoryById(id);
@@ -61,5 +75,45 @@ public class AdminController {
 		
 	}
 	
+	// product routes
+	@GetMapping("/admin/products")
+	public String getProducts(Model model) {
+		model.addAttribute("products" ,productService.getAllProduct());
+		return "products";
+	}
+	
+	@GetMapping("/admin/products/add")
+	public String getProductAdd(Model model) {
+		model.addAttribute("productDAO" ,new ProductDao());
+		model.addAttribute("categories",categoryService.getAllCategories());
+		return "productsAdd";
+	}
+	@PostMapping("/admin/products/add")
+	public String postProductAdd(@ModelAttribute("productDAO") ProductDao productDao, 
+			@RequestParam("productImage")MultipartFile file, @RequestParam("imgName") String imgName
+			)throws IOException {
+		
+		Product product = new Product();
+		product.setId(productDao.getId());
+		product.setName(productDao.getName());
+		product.setCategory(categoryService.getCategoryById(productDao.getCategoryId()).get());
+		product.setPrice(productDao.getPrice());
+		product.setDescription(productDao.getDescription());
+		product.setWeight(productDao.getWeight());
+		String imageUUID;
+		if(!file.isEmpty()) {
+			imageUUID = file.getOriginalFilename();
+			Path fileNameAndPath  = Paths.get(uploadDir, imageUUID);
+			Files.write(fileNameAndPath, file.getBytes());
+		}else {
+			imageUUID = imgName;
+		}
+		System.out.println(uploadDir);
+		
+		product.setImageName(imageUUID);
+		productService.addProduct(product);
+		
+		return "redirect:/admin/products";
+	}
 	
 }
