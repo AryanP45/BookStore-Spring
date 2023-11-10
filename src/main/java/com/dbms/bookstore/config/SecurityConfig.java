5040817632e59c782a00b40855b017b5ca4b75ad
Aryan <aryanitinpatil@gmail.com>
2023-11-10 18:56:34 +0530

    endpoint testing

--- src/main/java/com/dbms/bookstore/config/SecurityConfig.java
+++ src/main/java/com/dbms/bookstore/config/SecurityConfig.java
@@ -6,10 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,10 +15,7 @@ import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher.Builder;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
-import com.dbms.bookstore.model.CustomUserDetail;
 import com.dbms.bookstore.services.CustomUserDetailsService;
-
-import ch.qos.logback.core.pattern.color.BoldCyanCompositeConverter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -41,9 +36,8 @@ public class SecurityConfig {
 		return new MvcRequestMatcher.Builder(introspector);
 	}
 
-	@SuppressWarnings("removal")
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
 
 		http
 		.csrf(csrf -> csrf.disable())
@@ -53,9 +47,12 @@ public class SecurityConfig {
 						.requestMatchers(mvc.pattern("/register/**")).permitAll()
 						.requestMatchers(mvc.pattern("/images/**")).permitAll()
 						.requestMatchers(mvc.pattern("/productImages/**")).permitAll()
+//						Just for Testing
+//						.requestMatchers(mvc.pattern("/cart/**")).permitAll()
+//						.requestMatchers(mvc.pattern("/checkout")).permitAll()
 						.requestMatchers(mvc.pattern("/admin")).hasRole("ADMIN").anyRequest().authenticated().and()
 						.formLogin(login -> login.loginPage("/login").permitAll().failureUrl("/login?error=true")
-								.defaultSuccessUrl("/").usernameParameter("email").passwordParameter("password"))
+								.defaultSuccessUrl("/shop").usernameParameter("email").passwordParameter("password"))
 						.logout(logout -> logout.logoutRequestMatcher(mvc.pattern("/logout")).logoutSuccessUrl("/login")
 								.invalidateHttpSession(true).deleteCookies("JSESSIONID"))
 						.exceptionHandling(withDefaults());
@@ -68,15 +65,10 @@ public class SecurityConfig {
 
 		return http.build();
 	}
-	
-//	  @Bean
-//	    public WebSecurityCustomizer webSecurityCustomizer() {
-//	        return (web) -> web.ignoring().requestMatchers(mvc.pattern("/shop/**"),mvc.pattern("/register"));
-//	    }
 
-	
-	@Bean
-	public AuthenticationProvider authenticationProvider() {
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
 		authenticationProvider.setUserDetailsService(customUserDetailsService);
 		authenticationProvider.setPasswordEncoder(encoder);
