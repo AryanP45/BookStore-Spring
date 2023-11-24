14408533af0d37df8bf5f9d86b2e9af6d088897c
Aryan <aryanitinpatil@gmail.com>
2023-11-24 23:44:06 +0530

    search endpoint

--- src/main/java/com/dbms/bookstore/controllers/HomeController.java
+++ src/main/java/com/dbms/bookstore/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.dbms.bookstore.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,7 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.dbms.bookstore.global.GlobalData;
+import com.dbms.bookstore.model.Product;
 import com.dbms.bookstore.model.User;
 import com.dbms.bookstore.repository.UserRepository;
 import com.dbms.bookstore.services.CategoryService;
@@ -25,15 +30,15 @@ public class HomeController {
 	@GetMapping(path = { "/", "/home" })
 	public String home(Model model) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		
+
 		// authenticated user
 		if (authentication.isAuthenticated()) {
 			System.out.println(authentication.getPrincipal());
 			try {
-				User user= userRepository.findUserByEmail(authentication.getName()).get();
-				if(GlobalData.cart.isEmpty())
-				GlobalData.cart.addAll(user.getCartProducts());
-				
+				User user = userRepository.findUserByEmail(authentication.getName()).get();
+				if (GlobalData.cart.isEmpty())
+					GlobalData.cart.addAll(user.getCartProducts());
+
 			} catch (Exception e) {
 				// TODO: handle exception
 				e.printStackTrace();
@@ -46,13 +51,26 @@ public class HomeController {
 	}
 
 	@GetMapping("/shop")
-	public String shop(Model model) {
+	public String shop(Model model,@RequestParam(required = false) String search) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		System.out.println("Home page " + authentication.getName());
 		model.addAttribute("cartCount", GlobalData.cart.size());
 		model.addAttribute("categories", categoryService.getAllCategories());
-		model.addAttribute("products", productService.getAllProduct());
 		System.out.println("cartcountt : " + GlobalData.cart.size());
+		List<Product> products;
+		if(search == null) {
+			products = productService.getAllProduct();
+			System.out.println("Showing All products!!");
+		}else {
+			products = productService.getSearchedProducts(search);
+			System.out.println("Results searched for "+search);
+		}
+		if(products.isEmpty())
+			System.out.println("NO RESULTS FOUND");
+		else {
+			model.addAttribute("products",products);
+		}
+		
 		return "shop";
 	}
 
@@ -70,5 +88,7 @@ public class HomeController {
 		model.addAttribute("product", productService.getProductById(id).get());
 		return "viewProduct";
 	}
+
+//	@GetMapping("/shop")
 
 }
